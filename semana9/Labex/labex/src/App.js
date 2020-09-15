5955980dc632751b2ac837b05b0795045f43656f
luaanaapreiraa <luanapdsantos@gmail.com>
2020-09-15 16:46:45 -0300

    Semana9-aula34

---
+++ semana9/Labex/labex/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route, BrowserRoter} from 'react-router-dom';
+
+
+function App() {
+  return (
+   <BrowserRouter>
+   </BrowserRouter>
+  );
+}
+
+export default App;
