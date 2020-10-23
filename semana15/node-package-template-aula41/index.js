a045516fb38c5b31fe626627a0d7ae63d27c0a27
luaanaapreiraa <luanapdsantos@gmail.com>
2020-10-23 16:11:00 -0300

    estrutura

---
+++ semana15/node-package-template-aula41/index.js
@@ -0,0 +1,16 @@
+const countries = require("./countries")
+console.log(countries)
+
+//EXERCÍCIO 1
+//const nome = process.argv[2];
+//idade = Number(process.argv[3]);
+//const valor= Number("7");
+
+//console.log("Olá, ", nome + "! Você tem ", idade, "anos. Em sete anos você terá ", idade + valor);
+
+//EXERCÍCIO 2
+const valor1 = Number(process.argv[2]);
+const valor2 = Number(process.argv[3]);
+
+console.log(valor1 + valor2);
+
